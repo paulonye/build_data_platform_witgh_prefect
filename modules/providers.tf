5d6aec37a0279a60cfc2e7910f554926315fa108
paulonye <nwosupaul141@gmail.com>
2023-12-28 23:03:44 +0100

    Formatted tf file

--- modules/providers.tf
+++ modules/providers.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.10.0"
     }
   }
-  backend "gcs" { 
-      bucket  = "terraform-state-pb"
-    }
+  backend "gcs" {
+    bucket = "terraform-state-pb"
+  }
 }
 
 provider "google" {
   project = var.project
-  region = var.region
+  region  = var.region
 }
