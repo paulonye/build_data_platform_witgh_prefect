f79a3d3dc81eefb3180b18689a943ff2c56c682b
paulonye <nwosupaul141@gmail.com>
2023-12-28 23:03:52 +0100

    Formatted tf file

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,15 +1,15 @@
 variable "project" {
-  type = string
-  default = "chrome-epigram-386922"
+  type        = string
+  default     = "chrome-epigram-386922"
   description = "The Gcp Project Id"
 }
 
 variable "region" {
-    type = string
-    default = "us-east1"  
+  type    = string
+  default = "us-east1"
 }
 
 variable "repo_id" {
-    type = string
-    default = "prefect-repository"
+  type    = string
+  default = "prefect-repository"
 }
