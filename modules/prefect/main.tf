e59d84d831afca91a1eda51f3526216349a0e454
paulonye <nwosupaul141@gmail.com>
2023-12-30 23:09:46 +0100

    Modified prefect workpool resource

--- modules/prefect/main.tf
+++ modules/prefect/main.tf
@@ -15,8 +15,9 @@
 # }
 
 resource "prefect_work_pool" "example" {
-  name         = "cloud-run-job-infra"
-  type         = "cloud-run:push"
-  paused       = false
-  workspace_id = var.workspace_id
+  name         = var.workpool_name
+  type         = var.workpool_type
+  paused       = var.workpool_status
+  workspace_id = var.prefect_workspace_id
+  base_job_template = var.base_job_template
 }
