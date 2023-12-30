8555a86c446cee12b9b1ec02e6a51304c1f8b39d
paulonye <nwosupaul141@gmail.com>
2023-12-30 23:03:53 +0100

    Added prefect variables

--- modules/variables.tf
+++ modules/variables.tf
@@ -13,3 +13,42 @@ variable "repo_id" {
   type    = string
   default = "prefect-repository"
 }
+
+variable "prefect_workspace_id" {
+  description = "Workspace id of the Prefect Account"
+  default     = "853697d1-0a7b-4531-bb9e-099475f0dd3f"
+}
+
+variable "prefect_api_key" {
+  description = "Api key that gives access to Prefect Account"
+  default     = "pnu_0bWwwVKkReDIY6b2LdX4PUKWUbSVKW4bOKGq"
+}
+
+variable "prefect_account_id" {
+  description = "Account id of your Prefect Account"
+  default     = "1e75118d-08d5-4d97-9250-670641adb003"
+}
+
+variable "base_job_template" {
+  description = "Json configuration file for workpool"
+  default     = "./base-job-template.json"
+  type        = string
+}
+
+variable "workpool_name" {
+  description = "Name given to the workpool"
+  default     = "cloud-run-job-infra"
+  type        = string
+}
+
+variable "workpool_type" {
+  description = "The Workpool type"
+  default     = "cloud-run:push"
+  type        = string
+}
+
+variable "workpool_status" {
+  description = "Whether the workpool is paused or not"
+  type        = bool
+  default     = false
+}
