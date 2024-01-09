ee8fe82d060014541b8258d8e5a78b9dd2e0eae5
paulonye <nwosupaul141@gmail.com>
2024-01-09 16:28:18 +0100

    Modified fargate configuration values

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -100,14 +100,14 @@ resource "aws_ecs_task_definition" "task" {
           "hostPort": 5000
         }
       ],
-      "memory": 1024,
+      "memory": 2,
       "cpu": 1024
     }
   ]
   DEFINITION
   requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
   network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
-  memory                   = 1024        # Specify the memory the container requires
+  memory                   = 2        # Specify the memory the container requires
   cpu                      = 1024         # Specify the CPU the container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
