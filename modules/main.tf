9456d532732f90bf1b471d53d54740bd0f1430b0
paulonye <nwosupaul141@gmail.com>
2024-01-10 15:31:11 +0100

    Added cloud-run-prefect-workpool resource and also moved outputs to outputs file

--- modules/main.tf
+++ modules/main.tf
@@ -7,11 +7,22 @@ module "gcp" {
 
 module "prefect_ecs_work_pool" {
   source               = "./prefect"
-  workpool_name        = var.workpool_name
-  workpool_type        = var.workpool_type
+  workpool_name        = "aws-ecs-infra"
+  workpool_type        = "ecs:push"
   workpool_status      = var.workpool_status
   prefect_workspace_id = var.prefect_workspace_id
-  base_job_template    = file(var.base_job_template)
+  base_job_template    = file(var.aws_base_job_template)
+  prefect_api_key      = var.prefect_api_key
+  prefect_account_id   = var.prefect_account_id
+}
+
+module "prefect_cloud_run_work_pool" {
+  source               = "./prefect"
+  workpool_name        = "cloud-run-job-infra"
+  workpool_type        = "cloud-run:push"
+  workpool_status      = var.workpool_status
+  prefect_workspace_id = var.prefect_workspace_id
+  base_job_template    = file(var.gcp_base_job_template)
   prefect_api_key      = var.prefect_api_key
   prefect_account_id   = var.prefect_account_id
 }
@@ -25,29 +36,4 @@ module "aws" {
   aws_region         = var.aws_region
   aws_access_key     = var.aws_access_key
   aws_secret_key     = var.aws_secret_key
-}
-
-output "vpc_id" {
-  value = module.aws.vpc_id
-}
-
-output "ecs_cluster_arn" {
-  value = module.aws.ecs_cluster_arn
-}
-
-output "task_definition_arn" {
-  value = module.aws.task_definition_arn
-}
-
-
-output "iam_taskexecution_role_arn" {
-  value = module.aws.iam_taskexecution_role_arn
-}
-
-output "ecr_repo_url" {
-  value = module.aws.ecr_repo_url
-}
-
-output "ecr_repo_name" {
-  value = module.aws.ecr_repo_name
 }
