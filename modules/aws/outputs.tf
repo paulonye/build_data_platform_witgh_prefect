750b8d019c600cd755b18e3c2f8c953088f537f6
paulonye <nwosupaul141@gmail.com>
2024-01-15 13:09:03 +0100

    Adjusted output names

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -1,23 +1,23 @@
-output "vpc_id" {
+output "aws_vpc_id" {
   value = aws_vpc.aws-vpc.id
 }
 
-output "ecs_cluster_arn" {
+output "aws_ecs_cluster_arn" {
   value = aws_ecs_cluster.aws-ecs-cluster.arn
 }
 
-output "iam_taskexecution_role_arn" {
+output "aws_iam_taskexecution_role_arn" {
   value = aws_iam_role.ecsTaskExecutionRole.arn
 }
 
-output "ecr_repo_url" {
+output "aws_ecr_repo_url" {
   value = aws_ecr_repository.aws-ecr.repository_url
 }
 
-output "ecr_repo_name" {
+output "aws_ecr_repo_name" {
   value = aws_ecr_repository.aws-ecr.name
 }
 
-output "task_definition_arn" {
+output "aws_task_definition_arn" {
   value = aws_ecs_task_definition.task.arn
 }
